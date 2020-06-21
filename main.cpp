318879741d076a729ea4f13ffb51dec4bf1f88e7
syamamura0x00 <syamamura0x00@syamamura0x00.local>
2020-06-22 01:56:38 +0900

    :sparkles:: ビデオフィルタ機能

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "opencv2/opencv.hpp"
 
 #define APP_NAME "VIDUX"
-#define MONITOR_WINDOW_NAME "VIDUX - Minitor"
+#define MONITOR_WINDOW_NAME "VIDUX - Monitor"
 
 using namespace cv;
 
@@ -19,16 +19,32 @@ int main(int argc, char *argv[])
 
 
     cv::Mat frame;
+    cv::Mat filterBuf;
+
+    // cv::Mat frame = cv::Mat(cv::Size(640, 480), CV_8UC3);
+
     while(true)
     {
         // フレーム取得
         cap.read(frame);
-        //
-        //取得したフレーム画像に対して，クレースケール変換や2値化などの処理を書き込む．
-        //
+
+        // 正規化処理
+        // cv::cvtColor(frame, frame, cv::COLOR_BGR2BGRA);
+        // cv::cvtColor(frame, frame, CV_BGR2GRAY, 1);
+
+        cv::imshow("before", frame);
+
+
+        // 映像処理
+        // cv::GaussianBlur(frame, frame, Size(7,7), 1.5, 1.5);
+        // cv::medianBlur(frame, frame, 3);
+        cv::bilateralFilter(frame, filterBuf, 5, 76, 12, 4);
+        cv::cvtColor(filterBuf, frame, cv::COLOR_RGB2RGBA);
+
 
         // モニター表示
         cv::imshow(MONITOR_WINDOW_NAME, frame);
+        // cv::imshow("after", frame);
 
         const int key = cv::waitKey(1);
         if(key == 'q')
@@ -37,10 +53,16 @@ int main(int argc, char *argv[])
         }
         else if(key == 's')
         {
-            cv::imwrite("img.png", frame);
+            cv::imwrite("vidux-capture.jpg", frame);
         }
     }
+
+    // Window を開放
     cv::destroyAllWindows();
+
+    // キャプチャデバイスを開放
+    cap.release();
+
     return 0;
 
 
@@ -52,6 +74,7 @@ int main(int argc, char *argv[])
         Mat frame;
         cap >> frame; // get a new frame from camera
         cvtColor(frame, edges, COLOR_BGR2GRAY);
+
         GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         Canny(edges, edges, 0, 30, 3);
         imshow(MONITOR_WINDOW_NAME, edges);
