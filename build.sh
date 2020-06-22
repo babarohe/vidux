c1798605d0fbee2c0353b253baaff9719cc98b9b
syamamura0x00 <syamamura0x00@syamamura0x00.local>
2020-06-22 15:32:20 +0900

    :pencil2:: v4l2loopbackの記述を削除

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ g++ -O2 \
     main.cpp \
     capture.cpp \
     gui_main.cpp \
-    lib/v4l2loopback_cpp/v4l2sink.cpp \
     `pkg-config --cflags --libs opencv`  \
     `pkg-config --cflags --libs gtk+-3.0`  \
     -o vidux
+
+
+    # lib/v4l2loopback_cpp/v4l2sink.cpp \
